4ecfee90011dfc047f5c3439947546b90cf0615f
irfanghat <irfanghat@gmail.com>
2023-07-26 00:08:08 +0300

    Source files

---
+++ public/wp-content/themes/storyteller/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Teach2give</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <link rel="icon" type="image/x-icon" href="./favicon.ico"> -->
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <link rel="stylesheet" href="https://t2gstylesheets.s3.eu-north-1.amazonaws.com/style.css" type="text/css">
+</head>
+
+<body>
+  <!-- NAVIGATION -->
+  <nav class="navigation">
+    <!-- LOGO -->
+    <img src="./assets/images/logo.png" alt="logo" class="logo" id="logo">
+
+    <!-- NAV LINKS -->
+    <ul class="nav-links">
+      <li class="link-item">
+        <a class="link active">
+          Home
+        </a>
+      </li>
+      <li class="link-item">
+        <a class="link" href="./pages/about.html">
+          About Us
+        </a>
+      </li>
+      <li class="link-item">
+        <a class="link" href="#why-mentor">
+          Why Mentor?
+        </a>
+      </li>
+      <li class="link-item">
+        <a class="link">
+          Blog
+        </a>
+      </li>
+
+      <!-- CTA -->
+      <a class="cta">
+        <button class="cta-btn">
+          Donate Your Time
+          <i class="fa-solid fa-caret-right"></i>
+        </button>
+      </a>
+    </ul>
+
+    <!-- SIDE NAVIGATION -->
+    <a class="side-navigation-toggle">
+      <i class="bar-icon fa-solid fa-bars"></i>
+    </a>
+  </nav>
